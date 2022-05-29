4695d84bc1b83b04521c72065703a24c32769e20
bbrunoo <brunoedsanntos@gmail.com>
2022-05-29 13:20:17 -0300

    att2

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double euro = 5.13;
+
+Console.WriteLine("Digite a quantidade de euro para compra");
+int valor = Convert.ToInt32(Console.ReadLine());
+
+double eurocompra = (euro*valor);
+
+Console.WriteLine($"O valor em real é: {eurocompra}");
